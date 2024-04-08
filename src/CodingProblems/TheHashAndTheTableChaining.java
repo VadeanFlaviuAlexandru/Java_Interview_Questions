7b3c8d3972ff4afee8a3057901691a84007c4940
Alex <vadean.flaviu.alex@gmail.com>
2024-04-08 20:07:59 +0300

    hashtable and some other things i forgot

---
+++ src/CodingProblems/TheHashAndTheTableChaining.java
@@ -0,0 +1,86 @@
+package CodingProblems;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.lang.reflect.Array;
+
+public class TheHashAndTheTableChaining {
+
+    @Data
+    public class HashItem<Key, Value> {
+        private final Key key;
+        @NonNull
+        private Value value;
+        private HashItem<Key, Value> nextHashItem;
+    }
+
+    public class HashTable<Key, Value> {
+        public static final int max_map_size = 5; // this is only for chaining <=> LL size
+        private final HashItem<Key, Value>[] hashTable;
+        private int size;
+
+        public HashTable() {
+            hashTable = (HashItem<Key, Value>[]) Array.newInstance(HashItem.class, max_map_size);
+        }
+
+        private int hash(Key key) {
+            return key.hashCode() % max_map_size;
+        }
+
+        public void put(Key key, Value value) {
+            int index = hash(key);
+            HashItem<Key, Value> item = hashTable[index];
+            while (item != null) {
+                if (item.getKey().equals(key)) {
+                    item.setValue(value);//update
+                    return;
+                }
+                item = item.getNextHashItem();
+            }
+            HashItem<Key, Value> hashItem = new HashItem<>(key, value);
+            hashItem.setNextHashItem(hashTable[index]);
+            hashTable[index] = hashItem;
+            size++;
+        }
+
+        public Value get(Key key) {
+            int index = hash(key);
+            HashItem<Key, Value> item = hashTable[index];
+            while (item != null) {
+                if (item.getKey().equals(key)) {
+                    return item.getValue();
+                }
+                item = item.getNextHashItem();
+            }
+            return null;
+        }
+
+        public Value remove(Key key) {
+            int index = hash(key);
+            HashItem<Key, Value> item = hashTable[index];
+            HashItem<Key, Value> prev = null;
+
+            while (item != null) {
+                if (item.getKey().equals(key)) {
+                    break;
+                }
+                prev = item;
+                item = item.getNextHashItem();
+            }
+            if (item == null) {
+                return null;
+            }
+            if (prev == null) {
+                hashTable[index] = item.getNextHashItem();
+            } else {
+                prev.setNextHashItem(item.getNextHashItem());
+            }
+            size--;
+            return item.getValue();
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/CodingProblems/TheHashAndTheTableProbing.java
@@ -0,0 +1,96 @@
+package CodingProblems;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.lang.reflect.Array;
+
+public class TheHashAndTheTableProbing {
+    @Data
+    public class HashItem<Key, Value> {
+        private final Key key;
+        @NonNull
+        private Value value;
+    }
+
+    public class HashTable<Key, Value> {
+        public static final int max_map_size = 5; // this is only for chaining <=> LL size
+        private final HashItem<Key, Value>[] hashTable;
+        private int size;
+        private int capacity;
+
+        public HashTable() {
+            hashTable = (HashItem<Key, Value>[]) Array.newInstance(HashItem.class, max_map_size);
+            capacity = max_map_size;
+        }
+
+        private HashTable(int capacity) {
+            hashTable = (HashItem<Key, Value>[]) Array.newInstance(HashItem.class, capacity);
+            this.capacity = capacity;
+        }
+
+        private int hash(Key key) {
+            return key.hashCode() % capacity;
+        }
+
+        public void put(Key key, Value value) {
+            if (size >= capacity * 0.75) {
+                resize(2 * capacity);
+            }
+            int index = hash(key);
+            while (hashTable[index] != null) {
+                if (hashTable[index].getKey().equals(key)) {
+                    hashTable[index].setValue(value);
+                    return;
+                }
+                index = (index + 1) & capacity;
+
+            }
+            hashTable[index] = new HashItem<>(key, value);
+            size++;
+        }
+
+        public void resize(int newCap) {
+            HashTable<Key, Value> tempMap = new HashTable<>(newCap);
+            for (int i = 0; i < capacity; i++) {
+                if (hashTable[i] != null) {
+                    tempMap.put(hashTable[i].getKey(), hashTable[i].getValue());
+                }
+            }
+            hashTable = tempMap.getHashTable();
+            capacity = newCap;
+        }
+
+        public Value get(Key key) {
+            int index = hash(key);
+            while (hashTable[index] != null) {
+                if (hashTable[index].getKey().equals(key)) {
+                    return hashTable[index].getValue();
+                }
+                index = (index + 1) % capacity;
+            }
+            return null;
+        }
+
+        public Value remove(Key key) {
+            int index = hash(key);
+            while (!hashTable[index].getKey().equals(key)) {
+                index = (index + 1) % capacity;
+            }
+            Value toReturn = hashTable[index].getValue();
+            hashTable[index] = null;
+            size--;
+            while (hashTable[index = (index + 1) % capacity] != null) {
+                HashItem<Key, Value> nextItem = hashTable[index];
+                hashTable[index] = null;
+                size--;
+                put(nextItem.getKey(), nextItem.getValue());
+            }
+
+            return toReturn;
+        }
+
+    }
+
+
+}
